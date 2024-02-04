517febb4852cc30d243f743134ed224e0d8fbfe2
quent1f <155187674+quent1f@users.noreply.github.com>
2024-02-04 10:31:20 +0100

    Adding cleaner features

--- morpion.cpp
+++ morpion.cpp
@@ -36,6 +36,8 @@ public:
         cout << "c'est au tour de " << turn << "\n";
     }
 
+    // change the grid in place and checking game status 
+
     int play(int i, int j) {       // return 2 for draw, -1 or 1 for the winner, 0 for game not finished
         if (grid[i][j] == 0) {     // can play only is the case (i,j) is empty
             grid[i][j] = turn;
@@ -75,33 +77,70 @@ public:
         return 0; 
     }
 
+    void possibleMoves() {
+        int k = 0;
+        for (int i = 0; i<3; i++) {
+            for (int j = 0; j<3; j++) {
+                if (grid[i][j] == 0) {
+                    cout << k << " : jouer (" << i << "," << j << ")\n";
+                    k++;
+                }
+            }
+        }
+    }
 };
 
 
-void playGame() {
+int playGame() {                // plays a game, return -1 if -1 won, 1 if 1 won and 2 if it's a draw
     Game g;
     g.init();
-    int i = 0, j = 0;
-    g.printGrid();
-    cout << "Que voulez-vous jouer ?" << "\n";
-    cin >> i >> j;
-    int gameState = g.play(i, j);
-    while (gameState == 0) {
+    int i = 0, j = 0, gameState = 0;
+    do {
         g.printGrid();
-        cout << "Que voulez-vous jouer ?" << "\n";
+        g.possibleMoves();
         cin >> i >> j;
-        gameState = g.play(i,j);
-    }
+        int gameState = g.play(i,j);
+    } while (gameState == 0);
     g.printGrid();
     if (gameState != 2) {
-        cout << gameState<< " a gagné la partie!" << "\n";
+        cout << gameState << " a gagné la partie!" << "\n";
+        return gameState;
     }
     else {
         cout << "Match nul !" << "\n";
+        return 2;
     }
 }
 
+class Player() {
+    public: 
+
+    private: 
+
+
+}
+
 int main() {
-    playGame();    
+    cout << "test" << endl;
+    // Game g; 
+    // g.init();
+    // g.possibleMoves();
+    // playGame();    
+
+        // Mesures de temps 
+
+    // vector<vector<int>> m = init_rdm_matrix(1000);
+
+    
+    // auto start_time = chrono::high_resolution_clock::now();
+    // vector<vector<int>> res = mult_naif(m, m);
+
+    // auto end_time = chrono::high_resolution_clock::now();
+    // auto time = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+    // cout << "Time execution : " << (float)time.count()/1000 << " milisecondes" << endl; 
+
+    // Tests 
+
     return 0;
 }
